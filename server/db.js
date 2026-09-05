2b7320b6d3dc7c0630677702aa8c98ade020348b
Chukwuebuka Igboabuchukwu <chukwuebuka.igboabuchukwu@univaciti.com>
2026-09-05 10:18:35 +0100

    Support production database connection

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const poolConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    }
+  : {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    };
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
